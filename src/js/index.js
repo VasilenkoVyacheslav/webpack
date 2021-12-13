ab78fe14b11bd0d68a0696db85b18001f046283c
VasilenkoVyacheslav <vasilenko.vyacheslav@gmail.com>
2021-12-13 21:39:21 +0200

    Lesson 14

--- src/js/index.js
+++ src/js/index.js
@@ -1,4 +1,7 @@
 window.$ = window.jQuery = require('jquery');
+import Swiper, { Autoplay, Navigation } from 'swiper';
+
+Swiper.use([ Autoplay, Navigation ]);
 
 $(document).ready(function (){
 
@@ -7,11 +10,43 @@ $(document).ready(function (){
         autoplay: {
             delay: 5000,
         },
-        slidesPerView: 4,
+        breakpoints: {
+            580: {
+                slidesPerView: 2,
+                spaceBetween: 20
+            },
+            840: {
+                slidesPerView: 3,
+                spaceBetween: 30
+            },
+            1024: {
+                slidesPerView: 4,
+                spaceBetween: 30
+            }
+        },
+        slidesPerView: 1,
         spaceBetween: 30,
         navigation: {
             nextEl: '.swiper-button-next',
             prevEl: '.swiper-button-prev',
         },
     });
+
+    //Tabs
+    $('.tabs__button').on('click', function () {
+        $(".tabs .tabs__button").removeClass("active").eq($(this).index()).addClass("active");
+        $(".tabs__item").hide().eq($(this).index()).fadeIn();
+    }).eq(0).addClass("active");
+    $(".tabs__item").eq(0).fadeIn();
+})
+
+$(window).scroll(function () {
+    const headerHeight = $('.header').outerHeight();
+    const bannerHeight = $('.main-banner').outerHeight();
+    if($(window).scrollTop() >= (bannerHeight - headerHeight)) {
+        $('.header').addClass('sticky');
+    }
+    else {
+        $('.header').removeClass('sticky');
+    }
 })
